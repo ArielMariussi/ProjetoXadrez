b1797f1399ca01298628abc078f44173c3223d3c
Ariel Mariussi <arielmariussi@gmail.com>
2024-05-11 19:47:21 -0300

    Comecando a implementar o tabuleiro

--- Program.cs
+++ Program.cs
@@ -7,10 +7,9 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Posicao P;
-            P = new Posicao(3, 4);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine("Posicao: " + P);
+
             Console.ReadLine();
         }
     }
